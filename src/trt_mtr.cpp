2a994572d8824e7d2915a108df47be91e79922eb
Daniel Sanchez <danielsanchezaran@gmail.com>
2025-01-15 16:10:29 +0900

    delete blank space
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- src/trt_mtr.cpp
+++ src/trt_mtr.cpp
@@ -259,7 +259,6 @@ bool TrtMTR::postProcess(
 
   trajectories.clear();
   trajectories.reserve(num_target_);
-
   for (auto b = 0; b < num_target_; ++b) {
     const auto score_itr = h_out_score_.cbegin() + b * num_mode_;
     const std::vector<double> scores(score_itr, score_itr + num_mode_);
